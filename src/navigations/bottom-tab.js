42300bc4e1d0b4d738d907098b949d247baa9844
Thai Ne <meoconlonton2001@gmail.com>
2022-04-17 17:26:03 +0700

    create bottom Tab

--- src/navigations/bottom-tab.js
+++ src/navigations/bottom-tab.js
@@ -2,15 +2,42 @@ import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeView from '../views/home/home.view.js';
 import SettingView from '../views/setting/setting.view.js';
-
-const Stack = createNativeStackNavigator();
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Feather';
+import {useTheme} from 'react-native-paper';
+const Tab = createBottomTabNavigator();
+const HomeIcon = props => <Icon {...props} name="home" />;
+const SettingIcon = props => <Icon {...props} name="settings" />;
 
 const BottomTab = () => {
+  const theme = useTheme();
   return (
-    <Stack.Navigator initialRouteName='Home'>
-      <Stack.Screen name="Home" component={HomeView} />
-      <Stack.Screen name="Setting" component={SettingView} />
-    </Stack.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: theme.colors.surface,
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <HomeIcon color={color} size = {size}/>;
+          },
+        }}
+        component={HomeView}
+      />
+      <Tab.Screen
+        name="Setting"
+        options={{
+          tabBarIcon: ({color, size}) => {
+            return <SettingIcon color={color} size = {size}/>;
+          },
+        }}
+        component={SettingView}
+      />
+    </Tab.Navigator>
   );
 };
 
