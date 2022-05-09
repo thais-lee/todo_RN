dbad05479966261ae1efd7c0f084471509d7d3b1
thais-lee <meoconlonton2001@gmail.com>
2022-05-09 11:57:27 +0700

    add axios request

---
+++ src/utils/api.util.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+export const axiosMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+};
+
+export async function axiosRequest(config) {
+  const {url, method, data, params, token} = config;
+
+  const axiosConfig = {url, method, data, params};
+
+  const headers={
+    Authorization: `Bearer ${token}`,
+  };
+
+  if(token){
+    axios.headers = headers; 
+  }
+
+  const response = await axios(axiosConfig);
+}
