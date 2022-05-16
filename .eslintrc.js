1f850e5036df3532d8702639c9e3f2dd66c29a4c
thais-lee <meoconlonton2001@gmail.com>
2022-05-16 11:56:31 +0700

    config elsint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+    'prettier/react',
+  ],
 };
